e18c18beeab15d99287fb434e6516ac975c1edcc
Saurav Bedwal <sauravbedwal1234@gmail.com>
2025-05-11 04:58:29 +0530

    final commit

--- src/components/scriptButtons/ScriptButtons.jsx
+++ src/components/scriptButtons/ScriptButtons.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./ScriptButtons.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
